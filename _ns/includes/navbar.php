3f2869dc3f932b9a59aa7251c178e220a6bc7e04
Vidya Kaipa <vidyalkaipa@gmail.com>
2017-02-08 17:54:18 -0800

    Updated webpages to be more mobile responsive and matched partials CSS to the correct partial.

--- _ns/includes/navbar.php
+++ _ns/includes/navbar.php
@@ -10,8 +10,8 @@
       <div class="navbar-buttons">
         <a href="#menu-list" id="menu-trigger">Menu</a>
         <ul id="menu-list" class="btn-group">
-          <li class="services"><a href="/services" target="_blank">Services</a></li>
-          <li class="samples"><a href="/work" target="_blank">Work</a></li>
+          <li class="services"><a href="/services">Services</a></li>
+          <li class="samples"><a href="/work">Work</a></li>
           <li class="resume"><a href="/resumes/vidya-kaipa-marketing.pdf" target="_blank">Resume</a></li>
           <li class="contact"><a href="mailto:vidyalkaipa@gmail.com"><strong>Contact</strong></a></li>
         </ul>
